26ca26e5154e112c9bedfabb2722fb5f769514f6
hnoarcos <arcos1@churchofjesuschrist.org>
2021-06-02 11:19:45 -0600

    Ran swift package init command to create structure

---
+++ Tests/PEPhotoCropEditorTests/PEPhotoCropEditorTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import PEPhotoCropEditor
+
+    final class PEPhotoCropEditorTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(PEPhotoCropEditor().text, "Hello, World!")
+        }
+    }
